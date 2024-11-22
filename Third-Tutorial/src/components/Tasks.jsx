00ec27c8f034e436ac5a17d4f6707194723ef130
unknown <htonioni@outlook.com>
2024-11-22 15:40:44 -0300

    Added delete task function

--- Third-Tutorial/src/components/Tasks.jsx
+++ Third-Tutorial/src/components/Tasks.jsx
@@ -1,15 +1,26 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, X } from "lucide-react";
 
 function Tasks(props) {
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-            {props.tasks.map((tasks) => (
-                <li key={tasks.id} className="flex gap-2">
-                    <button className="bg-slate-400 w-full text-white p-2 rounded-md text-left">
-                        {tasks.title}
+            {props.tasks.map((task) => (
+                <li key={task.id} className="flex gap-2">
+                    <button 
+                        onClick={() => props.onTaskClick(task.id)} 
+                        className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${
+                            task.isCompleted && "line-through"
+                        }`}
+                    >
+                        {task.title}<br/>
                     </button>
                     <button className="bg-slate-400 p-2 rounded-md text-white">
                         <ChevronRightIcon />
+                    </button>
+                    <button 
+                        onClick={() => props.onDeleteTaskClick(task.id)}
+                        className="bg-slate-400 p-2 rounded-md text-white"
+                    >
+                        <X />
                     </button>
                 </li>
             ))}
