8422522aeb45eb420cb8172fb2ab323a8564ca7a
unknown <htonioni@outlook.com>
2024-11-21 21:15:46 -0300

    using useState

--- Third-Tutorial/src/App.jsx
+++ Third-Tutorial/src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-  const message = 'Hello, world!'
+import { useState } from "react";
 
+function App() {
+  // State (estado)
+  // Variavel React que forca o componente a ser renderizado novamente  
+  const [message, setMessage] = useState('Hello, world!')
+  
   return (
     <div>
       <h1>{message}</h1>
+      <button onClick={() => {
+        setMessage("Ola, foi clicado!")
+      }}
+      >Mudar mensagem</button>
+      <ul>
+        <li>test</li>
+        <li>test2</li>
+      </ul>
     </div>
   )
 };
