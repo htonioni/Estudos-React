6e084a39d48f854e8176c27cae31e119b0ecc049
unknown <htonioni@outlook.com>
2024-11-22 08:24:09 -0300

    Using Props

--- Third-Tutorial/src/App.jsx
+++ Third-Tutorial/src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
+import AddTasks from "./components/AddTasks"
+import Tasks from "./components/Tasks"
 
 function App() {
+  const [tasks, setTasks] = useState([{
+    id:1,
+    title:"Estudar Programacao",
+    description:"Estudar programacao para se tornar um dev.",
+    isCompleted: false
+  },
+  {
+    id:2,
+    title:"Estudar NodeJS",
+    description:"Estudar programacao para se tornar um dev.",
+    isCompleted: false
+  },
+  {
+    id:3,
+    title:"Estudar Espanhol",
+    description:"Estudar programacao para se tornar um dev.",
+    isCompleted: false
+  }
+])
 
   return (
-    <h1 className="text-red-500">
-      Hello world!
-    </h1>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de tarefas</h1>
+        <AddTasks />
+        <Tasks tasks={tasks} banana="this is bananas"/>
+      </div>
+    </div>
   );
 }
 
--- Third-Tutorial/src/components/Tasks.jsx
+++ Third-Tutorial/src/components/Tasks.jsx
@@ -1,5 +1,11 @@
-function Tasks(){
-
-};
+function Tasks(props) {
+    console.log(props);
+    return (
+        <div>
+            <p>This is my props with tasks: {props.tasks[0].id}</p>
+            <p>This is the other props: {props.banana}</p>
+        </div>
+    );
+}
 
 export default Tasks;
