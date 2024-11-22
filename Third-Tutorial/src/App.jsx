b066f1e17f2305c3dc5ea1180eb4d37068851a3d
unknown <htonioni@outlook.com>
2024-11-21 22:12:50 -0300

    using tailwind

--- Third-Tutorial/src/App.jsx
+++ Third-Tutorial/src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 
 function App() {
-  // State (estado)
-  // Variavel React que forca o componente a ser renderizado novamente  
-  const [message, setMessage] = useState('Hello, world!')
-  
+
   return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={() => {
-        setMessage("Ola, foi clicado!")
-      }}
-      >Mudar mensagem</button>
-      <ul>
-        <li>test</li>
-        <li>test2</li>
-      </ul>
-    </div>
-  )
-};
+    <h1 className="text-red-500">
+      Hello world!
+    </h1>
+  );
+}
 
 export default App;
