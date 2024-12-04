20d84270412e10cb3ee182947b8cca54d6f76417
unknown <htonioni@outlook.com>
2024-12-03 21:27:53 -0300

    Added task insertion function

--- Third-Tutorial/src/App.jsx
+++ Third-Tutorial/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTasks from "./components/AddTasks";
 import Tasks from "./components/Tasks";
+import {v4} from 'uuid'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -39,13 +40,23 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title: title,
+      description: description,
+      isCompleted: false
+    };
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de tarefas
         </h1>
-        <AddTasks />
+        <AddTasks onAddTaskSubmit={onAddTaskSubmit}/>
         <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
