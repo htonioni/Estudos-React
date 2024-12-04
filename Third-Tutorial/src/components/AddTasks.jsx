b5ac5cd04eb900f2550badf3c5c906de1ee80df0
unknown <htonioni@outlook.com>
2024-12-03 21:00:38 -0300

    addTasks component structure

--- Third-Tutorial/src/components/AddTasks.jsx
+++ Third-Tutorial/src/components/AddTasks.jsx
@@ -1,5 +1,21 @@
- function AddTasks() {
-    
-};
+function AddTasks() {
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o Titulo da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <input
+        type="text"
+        placeholder="Digite a descricao da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+        Adicionar
+      </button>
+    </div>
+  );
+}
 
- export default AddTasks;
+export default AddTasks;
