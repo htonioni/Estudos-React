926fc57c0d1ec9b2b6a4a026928ad1f48a345b59
Hugo Tonioni <htonioni@outlook.com>
2024-12-05 08:22:08 -0300

    Added func to add project to list

--- Fourth-Alura/app.js
+++ Fourth-Alura/app.js
@@ -1,11 +1,50 @@
 
-const item = document.getElementById("input-item");
+const itemConteudo = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
 
-botaoSalvarItem.addEventListener("click", (event) => {
-   event.preventDefault()
-   console.log(item.value);
-   console.log(event.target);
-   console.log(event.target.value);
-});
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(event) {
+   event.preventDefault();
+
+   const itemDaLista = document.createElement("li");
+
+   // item-lista-container
+   const containerItemLista = document.createElement("div");
+   containerItemLista.classList.add("item-lista-container");
+
+   const containerNomeDoItem = document.createElement("div");
+
+   const nomeDoItem = document.createElement("p");
+   nomeDoItem.innerText = itemConteudo.value;
+   containerNomeDoItem.appendChild(nomeDoItem);
+   containerNomeDoItem.classList.add("container-nome-compra");
+
+   const containerBotoes = document.createElement("div");
+   const botaoRemover = document.createElement("button");
+   const botaoEditar = document.createElement("button");
+   botaoRemover.classList.add("item-lista-button");
+   botaoEditar.classList.add("item-lista-button");
+
+   const imagemRemover = document.createElement("img");
+   imagemRemover.src = "img/delete.svg";
+   imagemRemover.alt = "Remover";
+   const imagemEditar = document.createElement("img");
+   imagemEditar.src = "img/edit.svg" 
+   imagemEditar.alt = "Editar"
+   
+
+   botaoRemover.appendChild(imagemRemover);
+   botaoEditar.appendChild(imagemEditar);
+   containerBotoes.appendChild(botaoRemover);
+   containerBotoes.appendChild(botaoEditar);
+
+
+   containerItemLista.appendChild(containerNomeDoItem);
+   containerItemLista.appendChild(containerBotoes);
+   itemDaLista.appendChild(containerItemLista);
+
+   listaDeCompras.appendChild(itemDaLista)
+}
 
