378f7416810771b5b3a90cde7da68e8371fb5203
unknown <htonioni@outlook.com>
2024-12-12 11:02:45 -0300

    added task page function

--- task-list-react/src/components/Tasks.jsx
+++ task-list-react/src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -14,7 +24,10 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
             {task.title}
             <br />
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => onSeeDetailsClick(task)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
           </button>
           <button
