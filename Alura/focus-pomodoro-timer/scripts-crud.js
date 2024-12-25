5e44640556056bb9e6cd27cfacfbda148e0a4c26
Hugo Tonioni <htonioni@outlook.com>
2024-12-25 18:08:01 -0300

    added task submit function

--- Alura/focus-pomodoro-timer/scripts-crud.js
+++ Alura/focus-pomodoro-timer/scripts-crud.js
@@ -1,11 +1,39 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
+const ulTarefas = document.querySelector(".app__section-task-list")
 
-tarefas = []
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+function criarElementoTarefa(tarefaCadastrada) {
+   const li = document.createElement("li");
+   li.classList.add("app__section-task-list-item");
+
+   const svg = document.createElement("svg");
+   svg.innerHTML = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>`
+
+   const paragrafo = document.createElement("p");
+   paragrafo.textContent = tarefaCadastrada.descricao;
+   paragrafo.classList.add("app__section-task-list-item-description");
+
+   const botao = document.createElement("button");
+   botao.classList.add("app_button-edit")
+   const imagemBotao = document.createElement("img");
+   imagemBotao.src = "./imagens/crud/edit.png";
+   botao.appendChild(imagemBotao);
+
+   li.appendChild(svg);
+   li.appendChild(paragrafo);
+   li.appendChild(botao);
+   return li
+}
 
 btnAdicionarTarefa.addEventListener("click", () => {
    formAdicionarTarefa.classList.toggle("hidden")
+
 })
 
 formAdicionarTarefa.addEventListener("submit", (event) => {
@@ -14,5 +42,14 @@ formAdicionarTarefa.addEventListener("submit", (event) => {
       descricao: textArea.value
    }
    tarefas.push(tarefaCadastrada);
+   const elementoTarefa = criarElementoTarefa(tarefaCadastrada)
+   ulTarefas.append(elementoTarefa);
    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+   textArea.value = ''
+   formAdicionarTarefa.classList.add('hidden')
 })
+
+tarefas.forEach(tarefaCadastrada => {
+   const elementoTarefa = criarElementoTarefa(tarefaCadastrada)
+   ulTarefas.append(elementoTarefa);
+});
