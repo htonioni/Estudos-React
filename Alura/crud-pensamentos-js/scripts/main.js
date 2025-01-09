ec9c0b8493fa3d10a480a45a56f6a97ec105d1a2
Hugo Tonioni <htonioni@outlook.com>
2025-01-09 09:35:20 -0300

    added pensamento POST and clear form function

--- Alura/crud-pensamentos-js/scripts/main.js
+++ Alura/crud-pensamentos-js/scripts/main.js
@@ -1,5 +1,30 @@
 import ui from "./ui.js"
+import api from "./api.js"
+
+const botaoCancelar = document.getElementById("botao-cancelar")
+
 
 document.addEventListener("DOMContentLoaded", () => {
    ui.renderizarPensamentos();
+
+   const formularioPensamento = document.getElementById("pensamento-form");
+   formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
+   botaoCancelar.addEventListener("click", manipularCancelamento);
 })
+
+async function manipularSubmissaoFormulario(event) {
+   event.preventDefault();
+   const id = document.getElementById("pensamento-id").value;
+   const conteudo = document.getElementById("pensamento-conteudo").value;
+   const autoria = document.getElementById("pensamento-autoria").value;
+   try {
+      await api.salvarPensamento({ conteudo, autoria })
+      ui.renderizarPensamentos()
+   } catch {
+      alert("Erro ao salvar pensamento")
+   }
+}
+
+function manipularCancelamento() {
+   ui.limparFormulario();
+}
