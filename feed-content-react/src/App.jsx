86eb9725b7efbfaa419d8ccda0d93c1b536aefbc
unknown <htonioni@outlook.com>
2025-01-31 19:59:48 -0300

    Add the filter component - static

--- feed-content-react/src/App.jsx
+++ feed-content-react/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Sidebar from './components/Siderbar'
 import BarraDePesquisa from './components/BarraDePesquisa'
+import Filtro from './components/Filtro'
 
 function App() {
 
   return (
     <div className='container'>
-      <Sidebar/>
-      <BarraDePesquisa />
+      <Sidebar />
+      <div className='container-content'>
+        <BarraDePesquisa />
+        <Filtro />
+      </div>
     </div>
   )
 }
---
+++ feed-content-react/src/components/Filtro/index.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+export default function Filtro() {
+    return (
+        <section className="container-filtro">
+            <ul>
+                <li>Programacao</li>
+            </ul>
+            <button>Limpar tudo</button>
+        </section>
+    )
+}
