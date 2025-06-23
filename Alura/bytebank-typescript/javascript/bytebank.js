bc7e2480ca8b93460bb8d5085212cdf50c3dfb8e
htonioni <htonioni@outlook.com>
2025-06-23 09:53:55 -0300

    feat: added deposit and pay functionalities

--- Alura/bytebank-typescript/javascript/bytebank.js
+++ Alura/bytebank-typescript/javascript/bytebank.js
@@ -1,15 +1,39 @@
-const saldo = document.querySelector('.saldo-valor .valor');
-const elementoFormulario = document.querySelector(".block-nova-transacao form")
-const newSaldo = document.querySelector("form #valor");
-let valor = 50333
+let saldo = 3000;
+const elementoSaldo = document.querySelector('.saldo-valor .valor');
+elementoSaldo.textContent = saldo;
 
-saldo.innerText = valor
+const elementoFormulario = document.querySelector(".block-nova-transacao form")
 
 elementoFormulario.addEventListener("submit", (event) =>{
    event.preventDefault();
-   if (!elementoFormulario.checkValidity()){
-      alert("Preencha o formulário!")
+   // if (!elementoFormulario.checkValidity()){
+      //    alert("Preencha todos os campos da transação!")
+      //    return
+      // }
+      
+   const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+   const inputValor = elementoFormulario.querySelector("#valor");
+   const inputData = elementoFormulario.querySelector("#data")
+
+   let tipoTransacao = inputTipoTransacao.value;
+   let valor = inputValor.value;   
+   let data = inputData.value;
+
+   if (tipoTransacao === "Depósito") {
+      saldo += Number(valor)
+   } else if (tipoTransacao === "Transferência" || tipoTransacao === "Pagamento de Boleto"){
+      saldo -= valor
    } else {
-      saldo.innerText = newSaldo.value
+      alert("Transação Inválida!");
+      return
    }
+   elementoSaldo.textContent = saldo
+
+   const novaTransacaoObj = {
+      tipoTransacao: tipoTransacao,
+      valor: valor,
+      data: data
+   }
+   console.log(novaTransacaoObj);
+   elementoFormulario.reset();
 })
