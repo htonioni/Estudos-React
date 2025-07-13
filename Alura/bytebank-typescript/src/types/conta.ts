292ebf0bc979152a6a8fd96d11e0c092e742ae01
Your name here <htonioni@outlook.com>
2025-07-13 17:34:32 -0300

    feat: localStorage implementation

--- Alura/bytebank-typescript/src/types/conta.ts
+++ Alura/bytebank-typescript/src/types/conta.ts
@@ -1,45 +1,58 @@
 import { Transacao } from "./Transacao.js";
 import { TiposTransacao } from "./TiposTransacao.js";
 
-let saldo: number = 3000;
+// let saldo: number = 3000;
+let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0
+const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
+    if (key === "data") {
+        return new Date(value);
+    }
 
-function debitar(valor: number): void{
-    if (valor <= 0){
+    return value;
+}) || [];
+
+function debitar(valor: number): void {
+    if (valor <= 0) {
         throw new Error("O valor deve ser maior que 0")
     } else if (valor > saldo) {
         throw new Error("O valor é maior que o saldo disponível")
     }
     saldo -= valor;
+    localStorage.setItem("saldo", saldo.toString())
 }
 
-function depositar(valor: number): void{
-    if (valor <= 0){
+function depositar(valor: number): void {
+    if (valor <= 0) {
         throw new Error("O valor a ser depositado deve ser maior que 0")
     }
     saldo += valor;
+    localStorage.setItem("saldo", saldo.toString())
 }
 
 const Conta = {
-        getSaldo() {
-            return saldo;
-        },
+    getSaldo() {
+        return saldo;
+    },
 
-        getDataAcesso(): Date {
-            return new Date();
-        },
+    getDataAcesso(): Date {
+        return new Date();
+    },
 
-        registrarTransacao(novaTransacao: Transacao): void {
-            if (novaTransacao.tipoTransacao == TiposTransacao.DEPOSITO) {
-                depositar(novaTransacao.valor);
-            } else if (novaTransacao.tipoTransacao == TiposTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TiposTransacao.PAGAMENTO_BOLETO) {
-                debitar(novaTransacao.valor);
-            } else {
-                throw new Error("Transação Inválida!");
-            }
-
-            console.log("Esse eh o nova transacao: " + novaTransacao);
-
+    registrarTransacao(novaTransacao: Transacao): void {
+        if (novaTransacao.tipoTransacao == TiposTransacao.DEPOSITO) {
+            depositar(novaTransacao.valor);
+        } else if (novaTransacao.tipoTransacao == TiposTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TiposTransacao.PAGAMENTO_BOLETO) {
+            debitar(novaTransacao.valor);
+        } else {
+            throw new Error("Transação Inválida!");
         }
+
+
+        transacoes.push(novaTransacao);
+        console.log("Esse eh : " + transacoes);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
+
     }
+}
 
 export default Conta;
